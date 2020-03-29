275afb757fc750100b87411c8e1cfa5869350f4f
carlaschneider <62518386+carlaschneider@users.noreply.github.com>
2020-03-30 02:17:37 +0300

    dailychallenge+ex

---
+++ W2D1/Exercices/dailychallengeW2D1/Daily.js
@@ -0,0 +1,20 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+undefined
+array.shift(0);
+"Banana"
+array.push("Kiwi");
+4
+array
+(4) ["Apples", "Oranges", "Blueberries", "Kiwi"]
+array.shift(0)
+"Apples"
+array
+(3) ["Oranges", "Blueberries", "Kiwi"]
+array[0]= "kiwi";
+"kiwi"
+array[1]= "Oranges"
+"Oranges"
+array[2]= "Blueberries";
+"Blueberries"
+array
+(3) ["kiwi", "Oranges", "Blueberries"]
---
+++ W2D1/Exercices/exercice1/Exercice1.js
@@ -0,0 +1,47 @@
+Exercice 1 
+A)
+5 + "34" = 534
+5 - "4" = 1
+10 % 5 = 0
+5 % 10 = 5
+"Java" "Script" = " JavaScript"
+" " + " " = " "
+" " + 0 = "0"
+true + true = 2
+true + false = 1
+false + true = 1
+false - true = -1
+3 - 4 = -1
+"Bob" - "Bill" = NaN
+
+B)
+5 >= 1 : true
+0 === 1 : false
+4 <= 1 : false
+1 != 1 : false
+"A" > "B" : false
+"B" < "C" : true
+"a" > "A" : true
+"b" < "A" : false
+true === false : false
+true != true : false
+
+C)
+"HI THERE! ITS " "SUNNY " "OUTSIDE"
+"HI THERE! ITS SUNNY OUTSIDE"
+
+Exercice 2
+let a = 34;
+undefined
+let b = 21
+undefined
+a = 2
+2
+a + b
+23
+let c = 2
+undefined
+let c = a
+undefined
+c + b
+23
